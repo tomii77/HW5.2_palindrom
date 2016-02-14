828ea6cedeade488780bd025ea5c626e127a8e3e
tomii77 <bratina.tomi@gmail.com>
2016-02-14 17:19:52 +0100

    first commit

---
+++ pali.php
@@ -0,0 +1,24 @@
+<?php
+	$myStr = $_POST['pali'];	
+	//spet ta fora s šumniki, če uporabim regex, mi zbriše tudi šumnike... tko da... tole je zaenkrat rešitev :(
+	$withIgn = ", with unchecked \"<i>ignore punctuation</i>\"";
+	$myStr1 = $myStr;
+	$punct = array("+",",",".","-","'","\"","&","!","?",":",";","#","~","=","/","$","£","^","(",")","_","<",">");
+	if(isset($_POST['test'])){	
+		$myStr1 = str_replace($punct,'', $myStr1);
+		$withIgn = ", with checked \"<i>ignore punctuation</i>\"";
+	}
+	$newStr = utf8strrev($myStr1);
+	if(strtolower(str_replace(' ', '',$newStr)) == strtolower(str_replace(' ', '',$myStr1))){
+		$is = "IS";
+	}
+	else{ $is = "IS NOT";
+	}
+	echo "You have entered: <strong><i>$myStr</i></strong>$withIgn<br>Which is reversed: \"<strong>$newStr\"<br><br>
+		<i>$myStr</i><span style='color:red'> $is</span></strong> a palindrome!";
+	function utf8strrev($someStr){
+		preg_match_all('/./us', $someStr, $ar);
+		return join('', array_reverse($ar[0]));
+
+	};
+?>
